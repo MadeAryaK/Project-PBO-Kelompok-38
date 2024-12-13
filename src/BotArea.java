a4f022607dd221b7ebf6326f75df6a6687bb8ea3
F1D022149_Nabila Azkiya <148998877+nabilaazkiya@users.noreply.github.com>
2024-12-13 17:06:27 +0800

    Update BotArea.java

--- src/BotArea.java
+++ src/BotArea.java
@@ -1,61 +1,97 @@
-import java.awt.*;
-import javax.swing.*;
+// Source code is decompiled from a .class file using FernFlower decompiler.
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.image.ImageObserver;
+import javax.swing.JPanel;
+import javax.swing.Timer;
 
 public class BotArea extends JPanel {
-	private static final long serialVersionUID = 1L;
-	final int WIDTH = (830) / 2;
-	final int HEIGHT = 275;
+   private static final long serialVersionUID = 1L;
+   final int WIDTH = 415;
+   final int HEIGHT = 275;
+   Image icon;
+   int x = 157;
+   int y = 74;
+   int yVelocity = 1;
+   private boolean isMovingDown = true;
+   private int originalWidth = 200;
+   private int originalHeight = 200;
+   private int currentWidth;
+   private int currentHeight;
+   private boolean isZoomingIn;
 
-	Image icon;
-	int x = (int) (WIDTH - 101) / 2;
-	int y = (int) (HEIGHT - 201);
-	int yVelocity = 1;
-	private boolean isMovingDown = true;
+   BotArea() {
+      this.currentWidth = this.originalWidth;
+      this.currentHeight = this.originalHeight;
+      this.isZoomingIn = true;
+      this.setPreferredSize(new Dimension(415, 275));
+      this.setOpaque(true);
+      Timer timer = new Timer(30, (e) -> {
+         this.zoom();
+         this.repaint();
+      });
+      timer.start();
+   }
 
-	BotArea() {
-		setPreferredSize(new Dimension(WIDTH, HEIGHT));
-		setOpaque(true);
-	}
+   void setDead(Image icon) {
+      Image resized = icon.getScaledInstance(200, 200, 4);
+      this.icon = resized;
+      this.x = 147;
+      this.y = 37;
+      this.repaint();
+   }
 
-	void setDead(Image icon) {
-		Image resized = icon.getScaledInstance(200, 200, Image.SCALE_SMOOTH);
-		this.icon = resized;
-		x = (int) 147;
-		y = (int) 37;
-		repaint();
-	}
+   void setImage(Image icon) {
+      Image resized = icon.getScaledInstance(200, 200, 4);
+      this.icon = resized;
+      this.repaint();
+   }
 
-	void setImage(Image icon) {
-		Image resized = icon.getScaledInstance(200, 200, Image.SCALE_SMOOTH);
-		this.icon = resized;
-		repaint();
-	}
+   Image getImage() {
+      return this.icon;
+   }
 
-	Image getImage() {
-		return icon;
-	}
+   public void paint(Graphics g) {
+      super.paint(g);
+      Graphics2D g2D = (Graphics2D)g;
+      if (this.icon != null) {
+         int centerX = (415 - this.currentWidth) / 2;
+         int centerY = (275 - this.currentHeight) / 2;
+         g2D.drawImage(this.icon, centerX, centerY, this.currentWidth, this.currentHeight, (ImageObserver)null);
+      }
 
-	public void paint(Graphics g) {
+   }
 
-		super.paint(g);
+   public void move() {
+      if (this.y >= 275 - this.icon.getHeight((ImageObserver)null) || this.y < 0) {
+         this.isMovingDown = !this.isMovingDown;
+      }
 
-		Graphics2D g2D = (Graphics2D) g;
-		g2D.drawImage(icon, x, y, null);
-		repaint();
-	}
-	
-	public void move() {
+      if (this.isMovingDown) {
+         this.y += this.yVelocity;
+      } else {
+         this.y -= this.yVelocity;
+      }
 
-		if (y >= HEIGHT - icon.getHeight(null) || y < 0) {
-			isMovingDown = !isMovingDown;
-		}
+      this.repaint();
+   }
 
-		if (isMovingDown) {
-			y += yVelocity;
-		} else {
-			y -= yVelocity;
-		}
-		repaint();
-	}
+   public void zoom() {
+      if (this.isZoomingIn) {
+         this.currentWidth += 2;
+         this.currentHeight += 2;
+         if (this.currentWidth >= this.originalWidth + 50) {
+            this.isZoomingIn = false;
+         }
+      } else {
+         this.currentWidth -= 2;
+         this.currentHeight -= 2;
+         if (this.currentWidth <= this.originalWidth - 50) {
+            this.isZoomingIn = true;
+         }
+      }
 
+   }
 }
